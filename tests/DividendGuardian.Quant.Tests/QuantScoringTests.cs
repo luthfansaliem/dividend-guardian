a9b671f12d03ad4d127336466238333241883940
Luthfan Saliem <60121009+luthfansaliem@users.noreply.github.com>
2026-09-25 02:25:43 +0700

    BUILD 01: add tests/DividendGuardian.Quant.Tests/QuantScoringTests.cs

---
+++ tests/DividendGuardian.Quant.Tests/QuantScoringTests.cs
@@ -0,0 +1,15 @@
+using DividendGuardian.Quant;
+
+namespace DividendGuardian.Quant.Tests;
+
+public sealed class QuantScoringTests
+{
+    [Fact] public void YieldScoreUsesHistoricalMedian()
+    {
+        Assert.Equal(15m,QuantScoringEngine.ScoreYield(.06m,.05m));
+        Assert.Equal(9m,QuantScoringEngine.ScoreYield(.05m,.05m));
+    }
+
+    [Fact] public void SustainabilityIsCappedAt25()
+        => Assert.Equal(25m,QuantScoringEngine.ScoreSustainability(40m,40m,10));
+}
